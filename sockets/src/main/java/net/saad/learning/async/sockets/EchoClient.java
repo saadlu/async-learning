1a0068c6eef0a3027c3488b99768959386c36d20
Mashrur Mia (Sa'ad) <m.mia@psenterprise.com>
2018-11-24 17:49:56 +0000

    relocate repo
    Change-Id: I7b61c57e21c2fa7c1bf5202d255e32402e0301e8

---
+++ sockets/src/main/java/net/saad/learning/async/sockets/EchoClient.java
@@ -0,0 +1,69 @@
+package net.saad.learning.async.sockets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EchoClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EchoClient.class);
+
+    public static void main(String[] args) throws IOException {
+        try (Socket socket = new Socket("localhost", 4444)) {
+            echoWithServer(socket);
+        }
+    }
+
+    private static void echoWithServer(Socket serverSocket) {
+        StdPrintUtility.printlnOnScreen("connected to server");
+        echo(serverSocket);
+    }
+
+    private static void echo(Socket serverSocket) {
+        try (PrintWriter serverWriter = new PrintWriter(serverSocket.getOutputStream(), true)) {
+            echoWithServer(serverSocket, serverWriter);
+        } catch (IOException ex) {
+            LOGGER.error(ex.getMessage(), ex);
+        }
+    }
+
+    private static void echoWithServer(Socket serverSocket, PrintWriter toServer)
+            throws IOException {
+
+        try (BufferedReader serverReader = new BufferedReader(
+                    new InputStreamReader(serverSocket.getInputStream()))) {
+            echoWithServer(toServer, serverReader);
+        }
+
+    }
+
+    private static void echoWithServer(PrintWriter toServer, BufferedReader serverReader)
+            throws IOException {
+
+        try(BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in))) {
+            echoWithServer(toServer, serverReader, stdin);
+        }
+    }
+
+    private static void echoWithServer(PrintWriter toServer, BufferedReader serverReader,
+            BufferedReader stdin) throws IOException {
+
+        String userInput = promptUser(stdin);
+        while (userInput != null) {
+            toServer.println(userInput);
+            String serverResponse = serverReader.readLine();
+            StdPrintUtility.printlnFormatted("server says: %s", serverResponse);
+            userInput = promptUser(stdin);
+        }
+    }
+
+    private static String promptUser(BufferedReader stdin) throws IOException {
+        StdPrintUtility.printOnScreen("message to sent to server (Ctrl+D to end): ");
+        return stdin.readLine();
+    }
+}
